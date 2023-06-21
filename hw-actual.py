b778bd0780aa03f719b44856ba090df605e2cdd8
andrey g <andygal@mail.ru>
2023-06-21 18:56:10 +0400

    python homework #2

--- hw-actual.py
+++ hw-actual.py
@@ -1,93 +1,35 @@
-# Задача 2: Найдите сумму цифр трехзначного числа. 
-# 123 -> 6 (1 + 2 + 3)
-# 100 -> 1 (1 + 0 + 0)
+# Задача 10: На столе лежат n монеток. 
+# Некоторые из них лежат вверх решкой, а некоторые – гербом. 
+# Определите минимальное число монеток, которые нужно перевернуть,
+# чтобы все монетки были повернуты вверх одной и той же стороной. 
+# Выведите минимальное количество монет, которые нужно перевернуть
+orel = 3
+reshka = 2
+print(f'Минимальное количество монет: {min(orel,reshka)}')
 
-def find_sum_digits(number):
-    ''' (int) -> int
-    
-    Return sum of digits of number. 
+# Задача 12: Петя и Катя – брат и сестра. 
+# Петя – студент, а Катя – школьница. Петя помогает Кате по математике. 
+# Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. 
+# Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
+# Помогите Кате отгадать задуманные Петей числа.
 
-    Precondition: number >= 0
-    >>> find_sum_digits(123)
-    6
+def guess_numbers(s, p):
+    ''' (int), (int) -> (int, int)
     '''
-    return 0 if number == 0 else number % 10 + find_sum_digits (number//10) 
+    for x in range(1, s//2 + 1):
+        if x * (s - x) == p:
+            return (x, s-x)
+    return (-1,-1)
 
-# Задача 6: Вы пользуетесь общественным транспортом? Вероятно, вы
-# расплачивались за проезд и получали билет с номером. Счастливым
-# билетом называют такой билет с шестизначным номером, где сумма
-# первых трех цифр равна сумме последних трех. Т.е. билет с номером
-# 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать
-# программу, которая проверяет счастливость билета.
+x, y = guess_numbers(12, 21)
+print('Петя загадал числа: ',*(x, y) if x > 0 else 'no solution')
 
-# 385916 -> yes
-# 123456 -> no
-def is_happy_number(number):
-    ''' (int) -> bool
-
-    Return True if sum of first 3 digits equals sum of second 3 digits.
-    Else return False.
-
-    Precondition: number consists 6 digits
-    >>> is_happy_number(385916)
-    True
-    >>> is_happy_number(123456)
-    False
-    '''
-    return find_sum_digits(number // 1000) == find_sum_digits(number % 1000)
-
-# Задача 4: Петя, Катя и Сережа делают из бумаги журавликов. Вместе
-# они сделали S журавликов. Сколько журавликов сделал каждый
-# ребенок, если известно, что Петя и Сережа сделали одинаковое
-# количество журавликов, а Катя сделала в два раза больше журавликов,
-# чем Петя и Сережа вместе?
-# 6 -> 1 4 1
-# 24 -> 4 16 4
-# 60 -> 10 40 10
-
-def zhuravliki(count):
-    ''' (int) -> list of int
-    
-    Return list of integer, where list[0] - quantity made by Petya, 
-    list[1] - Katya, list[2] - Seryozha. Return an empty list 
-    if the has no solutions in integers.
-    >>> zhuravliki(6)
-    [1, 4, 1]
-    >>> zhuravliki(24)
-    [4, 16, 4]
-    '''
-    return [] if count % 6 != 0 else [count // 6, count // 6 * 4, count // 6]
-
-# Задача 8: Требуется определить, можно ли от шоколадки размером n
-# × m долек отломить k долек, если разрешается сделать один разлом по
-# прямой между дольками (то есть разломить шоколадку на два
-# прямоугольника).
-# 3 2 4 -> yes
-# 3 2 1 -> no
-def is_choco_bar_devided(n, m, k):
-    ''' (int, int, int) -> bool
-
-    Return True if a chocolate bar is divided for k pieces by on motion.
-    Else return False.
-
-    Precondition: 0 < k < m * n
-    >>> is_choco_bar_devided(3, 2, 4)
-    True
-    >>> is_choco_bar_devided(3, 2, 1)
-    False
-    '''
-    return (k % n == 0 or k % m == 0) and (k < m * n)
-
-# Дано натуральное число. Требуется определить,
-# является ли год с данным номером високосным.
-def is_big_year(year):
-    ''' (int) -< bool
-
-    >>> is_big_year(2016)
-    True
-    >>> is_big_year(2000)
-    True
-    >>> is_big_year(1900)
-    False
-    '''
-    return year % 4 == 0 and not (year % 100 == 0 and year % 400 !=0)
+# Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), 
+# не превосходящие числа N.
+n = 1023
+print ('Степени двойки: ', end='')
+cur = 1
+while cur <= n:
+    print(cur, end=' ')
+    cur *=2
+print()
